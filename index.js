b92ac1e489ea2d1a85105b49c1fbadb13a4eb30f
Timalsinarabin <timalsinarabin11@gmail.com>
2026-01-12 13:26:01 +0545

    feat: offline support through dexie

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-const noteData = document.querySelector('.note');
-assignNote = document.querySelector('.container');
-document.querySelector('.addNote').addEventListener('click',()=>{
+import Dexie from "dexie";
+
+const db = new Dexie('MyDatabase');
+db.version(1).stores({
+    notes: '++id,text'
+});
+
+async function addNote(text) {
+    return await db.notes.add({text})
+}
+
+async function delNote(id) {
+    await db.notes.delete(id);
+}
+
+async function display(){
+    let notes = await db.notes.toArray();
+    notes.forEach(note => {
+        createNote(note.id,note.text)
+    });
+}
+
+function createNote(id,text){
     const div = document.createElement('div');
     div.className='items';
+
     const btn = document.createElement('button');
     btn.innerText = 'X';
-    btn.addEventListener('click',()=>{
+
+    btn.addEventListener('click',async()=>{
+        await delNote(id);
         div.remove();
-    })
-    div.appendChild(document.createTextNode(noteData.value))
+    });
+    
+    div.appendChild(document.createTextNode(text));
     div.appendChild(btn);
     assignNote.appendChild(div);
+}
+const noteData = document.querySelector('.note');
+
+const assignNote = document.querySelector('.container');
+document.querySelector('.addNote').addEventListener('click',async ()=>{
+    const text = noteData.value;
+    const id = await addNote(text);
+    createNote(id,text);
 })
+await display();
