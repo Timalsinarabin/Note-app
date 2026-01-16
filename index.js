0873a4fd1c2093b1dd7f5df59d6ddcadb5c813fb
Timalsinarabin <timalsinarabin11@gmail.com>
2026-01-16 12:02:08 +0545

    chore: setup for service worker

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
+const APP={
+    SW: null,
+    init(){
+        if('serviceWorker' in navigator){
+            navigator.serviceWorker.register('/sw.js',{
+                scope: '/'
+            }).then(registration=>{
+                APP.SW = registration.installing || 
+                        registration.waiting ||
+                        registration.active;
+                console.log('service worker registered');
+            })
+        }
+        else{
+            console.log("service workers are not supported");
+        }
+    }
+};
+document.addEventListener('DOMContentLoaded',APP.init);
 
 const db = new Dexie('MyDatabase');
 db.version(1).stores({
