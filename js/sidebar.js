a3ddcd7a0797f50e657b1c2b6a6272e439491e08
Timalsinarabin <timalsinarabin11@gmail.com>
2026-01-28 13:06:30 +0545

    feat: remove background of sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,27 +4,50 @@ undone = document.querySelector('.undonenotes');
 done = document.querySelector('.donenotes');
 sidebar = document.querySelector('.sidebar');
 menu = document.querySelector('.menu');
+overlay = document.querySelector('.overlay');
+sideMenu = document.querySelector('.sidebar-menu');
 
+function closeSidebar() {
+    document.body.classList.remove('menu-active');
+    sidebar.style.display = 'none';
+    menu.style.display = 'block';
+}
+menu.addEventListener('click', () => {
+    document.body.classList.add('menu-active');
+    sidebar.style.display = 'block';
+    menu.style.display = 'none';
+}
+);
+overlay.addEventListener('click', () => {
+    closeSidebar();
+});
 allnotes.addEventListener('click', () => {
     display()
     sidebar.style.display = 'none';
     menu.style.display = 'block';
+    closeSidebar();
 });
 
 pinned.addEventListener('click', () => {
     display('pinned')
     sidebar.style.display = 'none';
     menu.style.display = 'block';
+    closeSidebar();
 }); 
 
 undone.addEventListener('click', () => {
     display('undone')
     sidebar.style.display = 'none';
     menu.style.display = 'block';
+    closeSidebar();
 });
 
 done.addEventListener('click', () => {
     display('done')
     sidebar.style.display = 'none';
     menu.style.display = 'block';
+    closeSidebar();
 }); 
+sideMenu.addEventListener('click', () => {
+    closeSidebar();
+});
