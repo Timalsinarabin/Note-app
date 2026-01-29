310770414c412bbba8d1a0c17995d0fe715009c7
Timalsinarabin <timalsinarabin11@gmail.com>
2026-01-29 19:16:59 +0545

    feat: add install button

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ let assets = [
     './assets/style/style.css',
     './assets/notes.png',
     './manifest.json',
+    './js/install.js',
     'https://unpkg.com/dexie@latest/dist/dexie.js'
 ];
 self.addEventListener('install',(ev)=>{
