fc8b0ec7a29cc9e27b4d98ed37e58f0e2f68e07e
Timalsinarabin <timalsinarabin11@gmail.com>
2026-01-30 09:55:25 +0545

    feat: Install button

--- js/swregister.js
+++ js/swregister.js
@@ -1,6 +1,6 @@
 
 const APP={
-    SW: null,
+    deferredInstall: null,
     init(){
         if('serviceWorker' in navigator){
             navigator.serviceWorker.register('./sw.js',{
@@ -11,9 +11,37 @@ const APP={
                         registration.active;
                 console.log('service worker registered',registration.scope);
             })
+            navigator.serviceWorker.addEventListener('message',({data})=>{
+                console.log("message from service worker",data);
+            });
+            window.addEventListener('appinstalled', (ev) => {
+                console.log("app installed", ev);
+            });
+            window.addEventListener('beforeinstallprompt',(ev)=>{
+                ev.preventDefault();
+                APP.deferredInstall = ev;
+                console.log("beforeinstallprompt");
+            });
+            let btn = document.querySelector('.installBtn');
+            btn?.addEventListener('click',APP.startChromeInstall); 
         }
         else{
             console.log("service workers are not supported");
+        }
+        
+    },
+    startChromeInstall(){
+            if(APP.deferredInstall){
+                console.log(APP.deferredInstall);
+                APP.deferredInstall.prompt();
+                APP.deferredInstall.userChoice.then((choice)=>{
+                    if(choice.outcome === 'accepted'){
+                        console.log("user accepted the install prompt");
+                    }
+                    else{
+                        console.log("user dismissed the install prompt");
+                    }
+            });
         }
     }
 };
