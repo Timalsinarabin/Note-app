cd62b6efc6a050029862f1ad669aa121468625b3
Timalsinarabin <timalsinarabin11@gmail.com>
2026-02-10 12:41:11 +0545

    feat: increased number of items oer page for larger devices

--- js/pagination.js
+++ js/pagination.js
@@ -1,25 +1,34 @@
 
 let currentPage = 1;
-const itemsPerPage = 4;
-
+var itemsPerPage;
+const width = window.innerWidth;
+if(width >780){
+    itemsPerPage = 6;
+}
+else{
+    itemsPerPage = 4;
+}
 async function createNote(id,text,color='white',mark = false,dec = false){
-    const col= document.createElement('div');
-    col.className = 'col'
-    const div = document.createElement('div');
-    div.className='items';
 
+    const div = document.createElement('div');
+    div.className='items pt-7 px-7 border-1 rounded relative overflow-hidden h-20 transition-all duration-300';
+
+    div.addEventListener('click',()=>{
+        div.classList.toggle('h-32');
+        div.classList.toggle('overflow-hidden');
+        div.classList.toggle('overflow-auto');
+    })
     const note = document.createElement('div');
-    note.className='note';
+    note.className='note text-2xl';
 
     const btns = document.createElement('div');
-    btns.className='btns';
+    btns.className='btns mt-5 flex gap-2';
 
     div.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
     note.style.textDecoration = dec;
 
     const btn = document.createElement('button');
-    btn.className='delbtn';
-    btn.className ='btn btn-danger';
+    btn.className='delbtn bg-danger absolute right-3 bottom-2 cursor-pointer';
     btn.innerText = '⌫';
     
     btn.addEventListener('click',async()=>{
@@ -29,11 +38,10 @@ async function createNote(id,text,color='white',mark = false,dec = false){
     });
 
     const btnDone = document.createElement('button');
-    btnDone.className='btnDone';
-    btnDone.className = "btn btn-primary"
+    btnDone.className='btnDone cursor-pointer bg-blue-500 box-border border-transparent rounded p-2 hover:bg-blue-600';
 
     const btnPin = document.createElement('div');
-    btnPin.className='btnPin';
+    btnPin.className='btnPin absolute right-2 top-2 cursor-pointer';
 
     if(mark=='done'){
         btnDone.style.display = 'none';
@@ -79,8 +87,7 @@ async function createNote(id,text,color='white',mark = false,dec = false){
 
     div.appendChild(note);
     div.appendChild(btns);
-    col.appendChild(div);
-    assignNote.appendChild(col);
+    assignNote.appendChild(div);
 }
 
 
